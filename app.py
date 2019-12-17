9f0ae1c3cef23623c999f6980e06ad72b1603391
Dave Eum <eumjungsik@gmail.com>
2019-12-16 20:05:14 -0500

    trying to fix flask app. adding codes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
@@ -36,41 +35,60 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+
+
 @app.route("/")
 def welcome():
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations"
-    )
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs</br>"
+        f"/api/v1.0/<start>/<end>"
+        
+        
+    )   
 
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     '''somethiong'''
-    # Calculate the date 1 year ago from the last data point in the database
-    lastdate = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    lastdate = dt.datetime.strptime(str(lastdate[0]), '%Y-%m-%d')
-
+   # Calculate the date 1 year ago from the last data point in the database
+    lastdate = session.query(Measurement.date).order_by(Measurement.date.desc()).all()
+    lastdate = lastdate[0]
+    lastdate = str(lastdate)
+    lastdate = lastdate.replace(',', '')
+    lastdate = lastdate.replace('(', '')
+    lastdate = lastdate.replace(')', '')
+    lastdate = lastdate.replace("'", '')
+    lastdate = lastdate.replace("", '')
+    lastdate = dt.datetime.strptime(lastdate, '%Y-%m-%d')
+    lastyear =  lastdate - dt.timedelta(days=365)
     # Perform a query to retrieve the data and precipitation scores
-    yearago =  lastdate - dt.timedelta(days=365)
+    query = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date >= lastyear).\
+    order_by(Measurement.date.asc()).all()
+    return jsonify({k:v for k,v in query})
+    
 
-    results = session.query(Measurement.date, Measurement.prcp)\
-        .filter(Measurement.date>=yearago).\
-        order_by(Measurement.date.asc()).all()
-
-    return jsonify({k:v for k,v in results})
 
 @app.route("/api/v1.0/stations")
 def stations():
-    ''' do code'''
+    query_active_station = session.query(Measurement.station, func.count(Measurement.id))\
+                       .group_by(Measurement.station)\
+                       .order_by(func.count(Measurement.id).desc()).all()
+    return jsonify({i:j for i,j in query_active_station.desc()})
 
 @app.route("/api/v1.0/<start>")
+
+    
+
+
 @app.route("/api/v1.0/<start>/<end>")
 def names(start = None, end = None):
 	
 	if not end:
 		end = datetime.now()
-
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
